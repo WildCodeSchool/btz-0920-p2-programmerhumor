58308ecfa26f223c111071e8d40a29283bcc17c4
Laura Glutron <lauraglutron@MacBook-Pro.local>
2020-10-27 16:55:03 +0100

    start to create first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+// import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Projet Reddit Programmer Humor</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="banner-container">
+        <ul>
+          <li>
+            <a href="home">
+              <img src="" alt="logo" />
+            </a>
+          </li>
+          <li>search bar</li>
+          <li>
+            <button type="button">sign up</button>
+          </li>
+          <li>
+            <button type="button">sign in</button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
