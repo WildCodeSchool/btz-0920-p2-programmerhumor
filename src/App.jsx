27e5f4d1b0f6d6d4885eebc23e5e997fed364769
nelly-chieng <nelly_chieng@yahoo.fr>
2020-10-27 17:09:37 +0100

    component started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+// import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Projet Reddit Programmer Humor</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <div>Contact</div>
+        <input type="button" className="footerButton" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
