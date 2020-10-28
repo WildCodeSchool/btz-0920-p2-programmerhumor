95e235e155ae6f9f4519a4ba24b5699bca72f541
MacBook Pro Anthony <macbookproanthony@macbook-pro-de-anthony.home>
2020-10-28 16:46:14 +0100

    add a openTextarea onclick  comment

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,17 +1,45 @@
-/* eslint-disable react/button-has-type */
 import './NewsFeed.css';
+import { Component } from 'react';
 
-function NewsFeed() {
-  return (
-    <div className="content-main">
-      <div className="box-1">
-        <div className="box-one-button">
-          <button className="like">like</button>
-          <button className="comment">comment</button>
-          <button className="share">share</button>
+class NewsFeed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTextAreaOpen: false,
+    };
+  }
+
+  render() {
+    const { isTextAreaOpen } = this.state;
+    return (
+      <div className="content-main">
+        <div className="box-1">
+          <div className="box-one-button">
+            <button type="button" className="like">
+              like
+            </button>
+            <button
+              type="button"
+              className="comment"
+              onClick={() => {
+                const newisTextAreaOpen = !isTextAreaOpen;
+                this.setState({ isTextAreaOpen: newisTextAreaOpen });
+              }}
+            >
+              comment
+            </button>
+            <button type="button" className="share">
+              share
+            </button>
+            {isTextAreaOpen && (
+              <div className="interface-comment">
+                <textarea name="" id="comment-text" cols="30" rows="10" />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default NewsFeed;
