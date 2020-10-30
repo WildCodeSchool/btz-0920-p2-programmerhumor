40125292efd2947832746c3921bca3feb5f279eb
MacBook Pro Anthony <macbookproanthony@macbook-pro-de-anthony.home>
2020-10-30 12:29:29 +0100

    add useState to like button and comment button

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,45 +1,51 @@
 import './NewsFeed.css';
-import { Component } from 'react';
+import { FiHeart, FiMessageCircle } from 'react-icons/fi';
+import { RiShareForwardLine } from 'react-icons/ri';
+import { useState } from 'react';
 
-class NewsFeed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isTextAreaOpen: false,
-    };
-  }
-
-  render() {
-    const { isTextAreaOpen } = this.state;
-    return (
-      <div className="content-main">
-        <div className="box-1">
-          <div className="box-one-button">
-            <button type="button" className="like">
-              like
-            </button>
-            <button
-              type="button"
-              className="comment"
-              onClick={() => {
-                const newisTextAreaOpen = !isTextAreaOpen;
-                this.setState({ isTextAreaOpen: newisTextAreaOpen });
-              }}
-            >
-              comment
-            </button>
-            <button type="button" className="share">
-              share
-            </button>
-            {isTextAreaOpen && (
-              <div className="interface-comment">
-                <textarea name="" id="comment-text" cols="30" rows="10" />
-              </div>
-            )}
+function NewsFeed() {
+  const [isTextareaOpen, setIsTextareaOpen] = useState(false);
+  const [isLikeClick, setIsLikeClick] = useState(false);
+  const [count, setCount] = useState(0);
+  return (
+    <div className="content-main">
+      <div className="box-1">
+        <div className="box-one-button">
+          <button
+            type="button"
+            className={isLikeClick ? 'like' : 'notLike'}
+            onClick={() => {
+              setIsLikeClick(!isLikeClick);
+              setCount(!isLikeClick ? count + 1 : count - 1);
+            }}
+          >
+            <FiHeart />
+          </button>
+          <button
+            type="button"
+            className="comment"
+            onClick={() => {
+              setIsTextareaOpen(!isTextareaOpen);
+            }}
+          >
+            <FiMessageCircle />
+          </button>
+          <button type="button" className="share">
+            <RiShareForwardLine />
+          </button>
+          {/*
+          <div>
+            <p>{count} Like</p>
           </div>
+          */}
+          {isTextareaOpen && (
+            <div className="interface-comment">
+              <textarea name="" id="comment-text" cols="30" rows="10" />
+            </div>
+          )}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default NewsFeed;
