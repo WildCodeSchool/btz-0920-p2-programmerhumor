a874cbbb123917e3280e84d3d734473b9273f7b2
nelly-chieng <nelly_chieng@yahoo.fr>
2020-11-03 12:23:59 +0100

    added newsfeed with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import NewsFeed2 from './components/NewsFeed2';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <NewsFeed2 />
+    </div>
+  );
 }
 
 export default App;
