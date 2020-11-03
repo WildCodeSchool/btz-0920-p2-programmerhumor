4a4ba3963e235932371da49dc9e7197c8bd3d017
nelly-chieng <nelly_chieng@yahoo.fr>
2020-11-03 11:21:16 +0100

    added filter button with reactstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Filter2 from './components/Filter2';
+
+function App() {
+  return (
+    <div className="App">
+      <Filter2 />
+    </div>
+  );
+}
+
+export default App;
