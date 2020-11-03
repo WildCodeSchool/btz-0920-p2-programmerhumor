56bdbefd7771c70f8678260dd6befdaa08876183
nelly-chieng <nelly_chieng@yahoo.fr>
2020-11-03 10:50:22 +0100

    added navbar with reactstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header2 from './components/Header2';
+
+function App() {
+  return (
+    <div className="App">
+      <Header2 />
+    </div>
+  );
+}
+
+export default App;
