b5c404cbb0f653328f42f779d5223de1fa4374a9
nelly-chieng <nelly_chieng@yahoo.fr>
2020-11-03 16:13:39 +0100

    added footer with reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Footer2 from './components/Footer2';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Footer2 />
+    </div>
+  );
 }
 
 export default App;
