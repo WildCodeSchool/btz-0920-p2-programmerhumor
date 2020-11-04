055e3432b0e6d82fa9a51ebbfa3ecf43046b99b4
Laura Glutron <lauraglutron@MacBook-Pro.local>
2020-11-04 10:55:50 +0100

    create useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header2 from './components/Header2';
 import Filter2 from './components/Filter2';
 import NewsFeed2 from './components/NewsFeed2';
 import Footer2 from './components/Footer2';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://www.reddit.com/r/programmerhumor.json').then((res) => {
+      setPost(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <Header2 />
       <Filter2 />
-      <NewsFeed2 />
+      <NewsFeed2
+        id={post.id}
+        userId={post.userId}
+        title={post.title}
+        body={post.body}
+        key={post.id}
+      />
       <Footer2 />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NewsFeed2.jsx
+++ src/components/NewsFeed2.jsx
@@ -1,17 +1,13 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
 import {
   Card,
   CardImg,
   CardText,
   CardBody,
   CardTitle,
-  CardSubtitle,
   Button,
 } from 'reactstrap';
 
-// eslint-disable-next-line no-unused-vars
-const NewsFeed2 = (props) => {
+const NewsFeed2 = (id, userId, title, body) => {
   return (
     <div className="w-50 h-50 p-3 mx-auto">
       <Card>
@@ -22,12 +18,8 @@ const NewsFeed2 = (props) => {
           alt="Card image cap"
         />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </CardText>
+          <CardTitle>{title}</CardTitle>
+          <CardText>{body}</CardText>
           <Button>Button</Button>
           <Button>Button</Button>
           <Button>Button</Button>
