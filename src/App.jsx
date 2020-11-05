952bcecefd80d3f14ca6f2f3f542da553253304c
nelly-chieng <nelly_chieng@yahoo.fr>
2020-11-05 14:34:06 +0100

    call api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Container, Row } from 'reactstrap';
 import Header2 from './components/Header2';
 import Filter2 from './components/Filter2';
 import NewsFeed2 from './components/NewsFeed2';
@@ -7,11 +8,15 @@ import Footer2 from './components/Footer2';
 import './App.css';
 
 const App = () => {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios.get('https://www.reddit.com/r/programmerhumor.json').then((res) => {
-      setPost(res.data); // chemin datas?
+      setPosts(
+        res.data.data.children.map((child) => {
+          return child.data;
+        })
+      );
     });
   }, []);
 
@@ -19,18 +24,22 @@ const App = () => {
     <div className="App">
       <Header2 />
       <Filter2 />
-      <NewsFeed2 // quelles props?
-        id={post.id}
-        userId={post.userId}
-        title={post.title}
-        body={post.body}
-      />
+      <Container>
+        <Row>
+          {posts.map((post) => {
+            return (
+              <NewsFeed2
+                title={post.title}
+                selftext={post.selftext}
+                author={post.author_fullname}
+              />
+            );
+          })}
+        </Row>
+      </Container>
       <Footer2 />
     </div>
   );
 };
 
 export default App;
-
-// map?
-// setTimeout?
